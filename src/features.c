5567888a12e24444eec5a7fbda02dd15a358b5af
Kévin <kevin.souben@etu.estia.fr>
2025-05-16 14:57:06 +0200

    EX Dimension Start

--- src/features.c
+++ src/features.c
@@ -1,6 +1,6 @@
 #include <estia-image.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "features.h"
 #include "utils.h"
 
@@ -13,4 +13,22 @@
 
 void helloWorld() {
     printf("Hello World !");
+}
+
+
+void dimension(char *source_path) {
+    unsigned char *data = NULL;
+    int width = 0, height = 0, channels = 0;
+
+    printf("Appel de dimension : %s\n", source_path);
+
+    if (read_image_data(source_path, &data, &width, &height, &channels)) {
+        printf("dimension: %d, %d\n", width, height);
+    } else {
+        fprintf(stderr, "Erreur : impossible de lire l'image %s\n", source_path);
+    }
+
+    if (data != NULL) {
+        free(data);
+    }
 }
