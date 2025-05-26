91fdfd25a7cf6420a77a05772658f1764fe332dc
Kévin <kevin.souben@etu.estia.fr>
2025-05-26 12:22:56 +0200

    Debut de stat_report

--- src/features.c
+++ src/features.c
@@ -88,3 +88,15 @@ void print_pixel( char *filename, int x, int y ) {
 
     free_image_data(data);
 }
+
+void stat_report(){
+    unsigned char *data = NULL;
+    int width = 0, height = 0, channels = 0;
+
+    read_image_data (filename, &data, &width, &height, &channels);
+    pixelRGB *pixel = get_pixel(data, width, height, channels, x, y);
+    printf("print_pixel (%d, %d): %d, %d, %d\n", x, y, pixel->R, pixel->G, pixel->B);
+
+    free_image_data(data);
+
+}
--- src/features.h
+++ src/features.h
@@ -16,4 +16,6 @@ void print_pixel( char *filename, int x, int y );
 
 void max_pixel(char *source_path);
 
+void stat_report(char *source_path);
+
 #endif
